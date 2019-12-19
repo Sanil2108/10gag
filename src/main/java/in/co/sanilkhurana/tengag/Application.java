7670408b073370642a66aeaa6a4873f9fab4c1fe
Sanil Khurana <sanilkhurana7@gmail.com>
2019-12-19 13:17:57 +0530

    Basic directory structure and toolchain setup

---
+++ src/main/java/in/co/sanilkhurana/tengag/Application.java
@@ -0,0 +1,11 @@
+package in.co.sanilkhurana.tengag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src') + '/main/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'src') + '/main/resources/static/built',
+        filename: 'bundle.js',
+        publicPath: '/app/'
+    },
+    module: {
+        rules: [{
+            test: /.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/react']
+                }
+            }
+        },
+        {
+            test: /\.css$/i,
+            use: ['css-loader'],
+        },
+        {
+            test: /\.svg$/,
+            loader: 'svg-inline-loader'
+        }]
+    },
+    mode: 'development',
+};
