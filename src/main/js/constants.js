8e9f157930d9c3a0db187029d382eeb53b06943a
Sanil Khurana <sanilkhurana7@gmail.com>
2020-04-14 22:59:46 +0530

    Adding S3

--- src/main/js/constants.js
+++ src/main/js/constants.js
@@ -1,5 +1,5 @@
 // API related information
-const PRODUCTION_ENV = true;
+const PRODUCTION_ENV = false;
 const PRODUCTION_URL = 'http://3.20.255.26:5000/api/';
 const DEVELOPMENT_URL = 'http://localhost:5000/api/';
 const BASE_URL = (PRODUCTION_ENV) ? PRODUCTION_URL : DEVELOPMENT_URL;
