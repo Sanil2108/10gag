01aacc3d60dfadcd06910304a4fc09c6ec41a7be
Sanil Khurana <sanilkhurana7@gmail.com>
2020-06-04 04:16:50 +0530

    Fixing start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt-get install -y default-jre;
-set -a;
-source ~/10gag/.env;
-set +a;
-sudo fuser -k 5000/tcp
+sudo apt-get install -y default-jre &&
+set -a &&
+source ~/10gag/.env &&
+set +a &&
+sudo fuser -k 5000/tcp &&
 java -jar ~/10gag/10gag-spring-boot.jar
