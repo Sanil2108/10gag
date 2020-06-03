29c4f5ec2a90a90f077aaeaf328626795fa01d3e
Sanil Khurana <sanilkhurana7@gmail.com>
2020-06-03 22:29:42 +0530

    Configured automated deployment

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+source variables.env
+mvn clean package spring-boot:repackage
+
+# Frontend deployment on S3
+ACCESS_KEY=$(cat ~/Documents/aws/mainInstanceRootKey.csv | grep -P "AWSAccessKeyId=[A-Za-z0-9]+" | cut -d "=" -f2)
+SECRET_KEY=$(cat ~/Documents/aws/mainInstanceRootKey.csv | grep -P "AWSSecretKey=[A-Za-z0-9]+" | cut -d "=" -f2)
+
+mkdir front-end-build
+cp -r target/classes/static/built/* front-end-build/
+cp -r target/classes/templates/* front-end-build/
+aws s3 sync ./front-end-build s3://10gag-frontend
+
+# Backend deployment on EC2
+scp -r -i ~/Documents/aws/main-instance.pem target/10gag-spring-boot.jar variables.env ubuntu@3.7.175.185:~/10gag/
+ssh -i -f ~/Documents/aws/main-instance.pem ubuntu@3.7.175.185 "sudo apt-get install -y default-jre; source ~/10gag/variables.env; java -jar ~/10gag/10gag-spring-boot.jar"
+
+echo "Deployed to http://10gag-frontend.s3-website.ap-south-1.amazonaws.com"
+# chromium http://10gag-frontend.s3-website.ap-south-1.amazonaws.com
--- src/main/js/constants.js
+++ src/main/js/constants.js
@@ -1,6 +1,6 @@
 // API related information
-const PRODUCTION_ENV = false;
-const PRODUCTION_URL = 'http://3.20.255.26:5000/api/';
+const PRODUCTION_ENV = true;
+const PRODUCTION_URL = 'http://3.7.175.185/10gag/api/';
 const DEVELOPMENT_URL = 'http://localhost:5000/api/';
 const BASE_URL = (PRODUCTION_ENV) ? PRODUCTION_URL : DEVELOPMENT_URL;
 
